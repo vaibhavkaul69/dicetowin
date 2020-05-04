d9a429639c42d7437bbe5c9b5a1f7e642fd25f9b
unknown <vaibhav.kaul2@gmail.com>
2020-05-05 00:26:53 +0530

    PWA created

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-/*
-GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
+ //Check if the service worker exists in the navigator object.
+ if ("serviceWorker" in navigator) 
+ {
+     window.addEventListener("load", function() 
+     {
+     navigator.serviceWorker
+         .register("../service-worker.js")
+         .then(res => console.log("service worker registered"))
+         .catch(err => console.log("service worker not registered", err))
+     })
+ }
 console.log('Cookie storage :'+document.cookie);
 
 //Get the name of player-1 from DOM
@@ -34,6 +35,18 @@ const main_wrapper=document.querySelector('.wrapper');
 
 //Get all the elements from DOM having Button as their tag name
 let allDOMButton=document.getElementsByTagName('button');
+
+//Get the game intro audio element from DOM
+const gameIntroAudio=document.getElementById('newGameIntro');
+
+//Get the player wins the game audio element from DOM
+const playerWinsAudio=document.getElementById('playerWins');
+
+//Get the roll the dice audio element from DOM
+const rollTheDiceAudio=document.getElementById('rollDiceSound');
+
+//Get the pass the turn audio element from DOM
+const passTurnAudio= document.getElementById('passTheTurn');
 
 //Define the current active player.
 //0- player-1
@@ -71,28 +84,12 @@ const initializeGame=()=>{
 const newGameBtn=document.querySelector('.btn-new');
 newGameBtn.addEventListener('click',function(){
     initializeGame();
-   window.open('index.html','_top',true);
-   
+    setTimeout(function(){
+        window.open('index.html','_top',true);
+    },2200);
+    playerWinsAudio.pause();
+   gameIntroAudio.play();
 });
-
-//Get the name of player-1 and player-2 from user.
-const  getPlayersName=()=>{
-    if(confirm('Want Custom Player Names?\nThe Custom Nick-Names Should be MAXIMUM of 6 Characters (Without Spaces)..\nElse the Remaining Portion of Name Will NOT Included.'))
-    {
-        player_1_name.textContent=prompt('Enter Player-1 Nick-Name :').substr(0,6);
-        player_2_name.textContent=prompt('Enter Player-2 Nick-Name :').substr(0,6);
-
-        if( player_1_name.textContent.length<1 && player_2_name.textContent.length<1)
-        {
-            player_1_name.textContent='Player 1';
-            player_2_name.textContent='Player 2';
-        }
-    }
-    else
-    {
-        return;
-    }
-}
 
 //Check the active player while playing.
 //Here we check which player has .active class as a class member.
@@ -101,11 +98,13 @@ const  getPlayersName=()=>{
 const checkActiveClassPlayer=(diceNumber)=>{
     if(main_wrapper.children[activePlayer].classList.contains('active'))
     {
+        //Returns the current value for player-2
         if(activePlayer){
             currentValue_1+=diceNumber;
             console.log(`current value is ${currentValue_1} and dice number is ${diceNumber}`);
             return currentValue_1;
         }
+         //Returns the current value for player-1
         else{
             currentValue_0+=diceNumber;
             console.log(`current value is ${currentValue_0} and dice number is ${diceNumber}`);
@@ -122,8 +121,6 @@ const checkActiveClassPlayer=(diceNumber)=>{
         allDOMButton[2].setAttribute('disabled','disabled');
     };
 
-
-
 //Add an event listener to the Roll Dice button
 const rollDiceBtn=document.querySelector('.btn-roll');
 
@@ -132,7 +129,7 @@ const rollDiceBtn=document.querySelector('.btn-roll');
         //2. Change dice images.
             //3. Change the current score of the active player
 rollDiceBtn.addEventListener('click',()=>{
-
+    rollTheDiceAudio.play();
     //1. Generate a random Number.
     const diceNumber=parseInt((Math.random()*6)+1);
    //console.log(diceNumber);
@@ -142,14 +139,29 @@ rollDiceBtn.addEventListener('click',()=>{
     if(diceNumber>1){
         diceImage.src=`images/dice-${diceNumber}.png`;
         console.log( diceImage.src);
+
           //3. Append the randomly generated number to the current score of the current active player.
         main_wrapper.children[activePlayer].children[2].children[1].textContent=checkActiveClassPlayer(diceNumber);
+        console.log("Active Player when number greater than 1 :"+activePlayer);
     }
     else{
         diceImage.src=`images/dice-${diceNumber}.png`;
         main_wrapper.children[activePlayer].children[2].children[1].textContent=0;
         currentValue_0=0;
         currentValue_1=0;
+
+        //If activePlayer gets a 1 on dice.
+        //Then the turn shift to the other player.
+        if(activePlayer==0){
+            activePlayer=1;
+            main_wrapper.children[activePlayer].classList.toggle('active');
+            main_wrapper.children[activePlayer-1].classList.remove('active');
+        }
+        else{
+            activePlayer=0;
+            main_wrapper.children[activePlayer].classList.toggle('active');
+                main_wrapper.children[activePlayer+1].classList.remove('active');
+        }
     }    
     
 });
@@ -162,12 +174,10 @@ const holdDiceBtn=document.querySelector('.btn-hold');
         //2. updates the current score to the global score of hat player.
     
         holdDiceBtn.addEventListener('click',()=>{
-
+            passTurnAudio.play();
             //Here if the current active player is player-1 
-            //then shift the active class to player-2 and if
-            //the active player is player-2 then shift the active
-            //class to again player-1.
-            //This will go on in vice-versa loop.
+            //then shift the active class to player-2 
+           
             if(activePlayer==0)
             {
                
@@ -179,6 +189,11 @@ const holdDiceBtn=document.querySelector('.btn-hold');
                 main_wrapper.children[activePlayer-1].classList.remove('active');
                 currentValue_0=0;
             }
+
+            
+            //and if the active player is player-2 then shift the active
+            //class to again player-1.
+             //This will go on in vice-versa loop.
             else
             {
                 globalValue_1+=parseInt(main_wrapper.children[activePlayer].children[2].children[1].textContent);
@@ -201,12 +216,14 @@ const holdDiceBtn=document.querySelector('.btn-hold');
 */
 setInterval(function(){
     if(globalValue_0>=100){
+        playerWinsAudio.play();
         disableRollAndHoldBtn();
          main_wrapper.children[0].children[0].textContent='Winner';
          main_wrapper.children[0].children[0].style.cssText=" font-weight: bolder;transform: scale(1.5);color:#0f9b0f;";
          document.cookie=globalValue_0;
     }
     else if(globalValue_1>=100){
+        playerWinsAudio.play();
         disableRollAndHoldBtn();
          main_wrapper.children[1].children[0].textContent='Winner';
          main_wrapper.children[1].children[0].style.cssText=" font-weight: bolder;transform: scale(1.5);color:#0f9b0f;";
@@ -218,5 +235,22 @@ setInterval(function(){
 //When the window loads then call in game initializer function to resre all the values.
 window.onload=()=>{
     initializeGame();
-    setTimeout(getPlayersName,1000);
+    
+    //When the page loads then after a minimum of 1 sec load the getPlayersName() function.
+    setTimeout(()=>{
+        alert('You Can Give Custom Names to the Players :)\n The Name Should be Less Than 7 Characters !');
+    },1000);
 };
+
+//Function to keep a check on players name.
+//It will return false and will not accept any character value
+//after the player's name length becomes grater than 7 characters (0-6).
+const playerNameEnter=()=>{
+    console.log(1);
+    if(player_1_name.textContent.length<=6 || player_2_name.textContent.length<=6){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const diceGame = "DiceToWin";
+const assets = [
+  'index.html',
+  'audio/coffin-song.mp3',
+  'audio/game-intro-audio.mp3',
+  'audio/roll-dice-click-sound.mp3',
+  'audio/sound-effect.mp3',
+  'css/style.css',
+  'images/back.jpg',
+  'images/dice-1.png',
+  'images/dice-2.png',
+  'images/dice-3.png',
+  'images/dice-4.png',
+  'images/dice-5.png',
+  'images/dice-6.png',
+  'images/favicon72x72.png',
+  'images/favicon96x96.png',
+  'images/favicon128x128.png',
+  'images/favicon144x144.png',
+  'images/favicon152x152.png',
+  'images/favicon192x192.png',
+  'images/favicon384x384.png',
+  'images/favicon512x512.png',
+  'js/app.js'
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(diceGame).then(cache => {
+      cache.addAll(assets)
+      .catch(err=>console.log(err))
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+      
+    )
+  })
